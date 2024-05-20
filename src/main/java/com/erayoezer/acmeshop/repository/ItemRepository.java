fe178425e89dfda79167319f2602158b2f2aef96
Eray Özer <erayozer91@protonmail.com>
2024-05-20 14:18:13 +0200

    add @Repository to ItemRepository

--- src/main/java/com/erayoezer/acmeshop/repository/ItemRepository.java
+++ src/main/java/com/erayoezer/acmeshop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.erayoezer.acmeshop.repository;
 
 import com.erayoezer.acmeshop.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
